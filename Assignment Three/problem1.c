46912ca2f97ba0dd9d582611ac59fa3d6669d24e
UsamaRefaat <osamarefaatsubm@gmail.com>
2025-05-15 16:30:57 +0300

    added Openmp Pragma for problem 1 and time calc

--- Assignment Three/problem1.c
+++ Assignment Three/problem1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include <omp.h>
+#include <time.h>
 
 int main (int argc , char** argv)
 {
@@ -34,6 +36,9 @@ for (int i = 0; i < n; i++)
 {
     res[i]=0;
 }
+clock_t start, end;
+start = clock();
+#pragma omp parallel for
 for(int i = 0 ; i<n; i++) 
 {
 for (int j = 0; j < n; j++)
@@ -41,12 +46,15 @@ for (int j = 0; j < n; j++)
     res[i]+= vec[j] * mat[i][j];
 }
 }
-
+end= clock();
+double total = (double) (end-start)/ CLOCKS_PER_SEC;
+printf("total time %f\n",total);
 printf("Resulting vector r: ");
 for (int j = 0; j < n; j++)
 {
     printf  ( "%d ", res[j] );
 }
+printf("\n");
 
 return 0;
 }
